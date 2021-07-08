bc9d6f561b03de31408dbb870710d7af1d30981b
Ryan <ljiangnote@gmail.com>
2021-07-07 17:16:22 -0700

    updates

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import React from 'react';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
+
 
 import { Switch, Route } from 'react-router-dom';
 
@@ -10,9 +12,13 @@ import { Switch, Route } from 'react-router-dom';
 function App() {
   return (
     <div>
-      <Route exact path = '/' component ={HomePage} />
-      <Route path = '/shop' component ={ShopPage} />
+      <Header />
+      <Switch>
+        <Route exact path = '/' component ={HomePage} />
+        <Route path = '/shop' component ={ShopPage} />
+      </Switch>
     </div>
+    
   );
 }
 
