f34942c47f5b2eb9a82ce907da3c0f2128e83099
Ryan <ljiangnote@gmail.com>
2021-07-08 19:00:22 -0700

    updated application with reselector

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -8,6 +8,7 @@ import { connect } from 'react-redux';
 import './cart-dropdown.styels.scss';
 import CartItem from '../cart-item/cart-item.component';
 
+import { selectCartItems  } from '../../redux/cart/cart.selectors'
 
 const CartDropdown = ({ cartItems}) => (
     <div className='cart-dropdown'>
@@ -17,14 +18,14 @@ const CartDropdown = ({ cartItems}) => (
             cartItems.map(cartItem => (
                 <CartItem key={cartItem.id}  item = {cartItem} />
             ))
-
         }
         <CustomButton>GO TO CHECKOUT</CustomButton>
     </div>
 );
 
-const mapStateToProps = ({cart: {cartItems}}) =>({
-    cartItems
-})
+const mapStateToProps = (state) =>({
+    cartItems: selectCartItems(state)
+});
+
 
 export default connect(mapStateToProps) (CartDropdown);
